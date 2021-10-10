ef98827cfbf6a6b3ff1228ffa26d2ec30d9e9e48
0vertex <>
2021-10-10 23:29:15 +0530

    Added supporting files

---
+++ Scripts/generate_documentation.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#  generate_documentation.sh
+#  CoreUIKit
+#
+#  Created by Manish
+#
+
+
+jazzy \
+--author_url="https://github.com/0vertex/" \
+--author="Manish" \
+--github_url="https://github.com/0vertex/CoreUIKit" \
+--theme=fullwidth \
+--output="../CoreUIKit/docs"
